5adc3332ddbdf5d179d75248a5ef514fcb0c6f35
Musthafa Faishal <musthafafaishal@gmail.com>
2022-02-05 10:30:10 +0700

    Update response body

--- server.js
+++ server.js
@@ -13,12 +13,18 @@ const requestListener = (request, response) => {
     if (method === 'GET') {
       response.statusCode = 200;
       // response bila client menggunakan GET
-      response.end('<h1>Ini adalah homepage</h1>');
+      response.end(
+        JSON.stringify({
+          message: 'Ini adalah homepage',
+        })
+      );
     } else {
       // response bila client tidak menggunakan GET
       response.statusCode = 400;
       response.end(
-        `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
+        JSON.stringify({
+          message: `Halaman tidak dapat diakses dengan ${method} request`,
+        })
       );
     }
   } else if (url == '/about') {
@@ -26,7 +32,11 @@ const requestListener = (request, response) => {
     if (method === 'GET') {
       // response bila client menggunakan GET
       response.statusCode = 200;
-      response.end('<h1>Halo! Ini adalah halaman about</h1>');
+      response.end(
+        JSON.stringify({
+          message: 'Halo! Ini adalah halaman about',
+        })
+      );
     } else if (method === 'POST') {
       // response bila client menggunakan POST
       let body = [];
@@ -39,19 +49,29 @@ const requestListener = (request, response) => {
         body = Buffer.concat(body).toString();
         const { name } = JSON.parse(body);
         response.statusCode = 200;
-        response.end(`<h1>Hai, ${name}! Ini adalah halaman about</h1>`);
+        response.end(
+          JSON.stringify({
+            message: `Hai, ${name}! Ini adalah halaman about`,
+          })
+        );
       });
     } else {
       // response bila client tidak menggunakan GET ataupun POST
       response.statusCode = 400;
       response.end(
-        `<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`
+        JSON.stringify({
+          message: `Halaman tidak dapat diakses menggunakan ${method} request`,
+        })
       );
     }
   } else {
     // TODO: (1) logika response bila url bukan '/' atau '/about'
     response.statusCode = 404;
-    response.end('<h1>Halaman tidak ditemukan!</h1>');
+    response.end(
+      JSON.stringify({
+        message: 'Halaman tidak ditemukan!',
+      })
+    );
   }
 
   // if (method == 'GET') {
